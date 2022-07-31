1771c6d3d2c558c349e80f5eb09da9cd27620f5b
Muhammad Arrezza Faiz Pratama <103143006+panda560@users.noreply.github.com>
2022-07-31 10:38:15 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['6283128734012']
+global.owner = ['6281273677810']
 
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -12,7 +12,7 @@ global.APIKeys = { // APIKey Here
   'https://melcanz.com': 'apikey',
 }
 
-global.wm = '© anonymous-chat'
+global.wm = '© anonymous-chat • By : M.A.F.PRATAMA'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text='
 
